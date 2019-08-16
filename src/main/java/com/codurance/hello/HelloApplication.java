c5515ba7da1b4d1f3c90420b7c02bebf29317f69
Fernando Guerra <fernandoguerra@Fernandos-MacBook-Pro.local>
2019-08-16 13:09:47 +0200

    Added docker

--- src/main/java/com/codurance/hello/HelloApplication.java
+++ src/main/java/com/codurance/hello/HelloApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, "--server.port=3434");
+		SpringApplication.run(HelloApplication.class, args);
 	}
 
 }
